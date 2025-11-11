af49e93f671bc1bf71e3b6fbff76342aac0cb44e
Alexandra DeLucia <aadelucia@gmail.com>
2025-11-11 23:58:24 +0000

    updated environment

--- factuality_omission_detection/evaluate_omission_results.py
+++ factuality_omission_detection/evaluate_omission_results.py
@@ -7,13 +7,17 @@ import argparse
 import logging
 import unicodedata
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 import pandas as pd
 import jsonlines
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
     recall_score,
-    f1_score
+    f1_score,
+    confusion_matrix
 )
 
 # Set up logging
@@ -37,15 +41,63 @@ model_display_order = {
 }
 
 
+def plot_confusion_matrix(cm: pd.DataFrame, output_path: str):
+    # Define the new labels
+    labels_x = ["Predicted Omission", "Predicted No Omission"]
+    labels_y = ["Actual Omission", "Actual No Omission"]
+
+    plt.figure(figsize=(5, 4), dpi=200)
+    sns.heatmap(
+        cm,
+        annot=True,
+        fmt="0.2f",
+        cmap="Blues",
+        xticklabels=labels_x,
+        yticklabels=labels_y,
+        vmax=1,
+        vmin=0,
+        linewidths=1.5,
+        linecolor='white'
+    )
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+
+def format_example(example: pd.Series) -> str:
+    """Format a single example for logging."""
+    if example.empty:
+        return "No example"
+    # Format the annotated and predicted omissions as numbered lists
+    if example.n_omissions_pred > 0:
+        pred = [f"{i + 1}. {omission}" for i, omission in enumerate(example.omissions_pred)]
+        pred = "\n\t".join(pred)
+    else:
+        pred = "No omissions predicted."
+    if example.n_omissions > 0:
+        gt = [f"{i + 1}. {omission}" for i, omission in enumerate(example.omissions)]
+        gt = "\n\t".join(gt)
+    else:
+        gt = "No annotated omissions."
+
+    out = f"ID: {example.name} Title: {example.title}\n"
+    out += f"Question: {example.question}\n"
+    out += f"Chatbot Response ({example.model}): {example.response}\n"
+    out += f"Severity: {example.omission_severity}\n"
+    out += f"Annotated Omissions:\n\t{gt}\n"
+    out += f"Predicted Omissions:\n\t{pred}\n"
+    return out
+
+
 def score_predictions(y_true: List[bool], y_pred: List[bool], pos_label: bool = True) -> pd.Series:
     """Compute classification metrics for binary predictions.
 
     Assumes positive class is True.
     """
     accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred, zero_division=0, pos_label=pos_label)
-    recall = recall_score(y_true, y_pred, zero_division=0, pos_label=pos_label)
-    f1 = f1_score(y_true, y_pred, zero_division=0, pos_label=pos_label)
+    precision = precision_score(y_true, y_pred, zero_division=0, pos_label=pos_label, average="binary")
+    recall = recall_score(y_true, y_pred, zero_division=0, pos_label=pos_label, average="binary")
+    f1 = f1_score(y_true, y_pred, zero_division=0, pos_label=pos_label, average="binary")
     row = pd.Series({
         "Acc.": accuracy,
         "Prec.": precision,
@@ -83,6 +135,12 @@ def load_medexpert_dataset(file_path: str) -> pd.DataFrame:
     df.omission_severity = df.omission_severity.map(normalize_severity)
     df.omission_severity = df.omission_severity.astype(SEVERITY_CATEGORIES)
     df["n_omissions"] = df.omissions.map(lambda x: len(x) if isinstance(x, list) else 0)
+
+    # Hack fix
+    err = df[(df.omission_severity > "No Omission") & (df.n_omissions == 0)]
+    if not err.empty:
+        logger.warning(f"Found {err.shape[0]} records with severity > 'No Omission' but 0 omissions. Fixing severity to 'No Omission'.")
+        df.loc[err.index, "omission_severity"] = "No Omission"
     return df
 
 
@@ -167,6 +225,7 @@ def main():
 
     # Compute and log metrics across domains and severity levels
     results_df = []
+    cm = None
     for domain, group_df in merged_df.groupby("domain", observed=True):
         # Compute metrics for each severity level within the domain
         for severity, df in group_df.groupby("omission_severity", observed=True):
@@ -185,6 +244,7 @@ def main():
             row["Severity"] = severity
             row["Domain"] = domain
             results_df.append(row)
+
         # Compute overall metrics for the domain
         row = score_predictions(
             group_df["label"].tolist(),
@@ -193,10 +253,32 @@ def main():
         row["Severity"] = "All-Err"
         row["Domain"] = domain
         results_df.append(row)
+
+        # Store confusion matrix for the "All" domain
+        if domain == "All":
+            cm = confusion_matrix(
+                group_df["label"].tolist(),
+                group_df["pred_label"].tolist(),
+                labels=[True, False],
+                normalize="true"
+            )
     results_df = pd.DataFrame(results_df)
     results_df["Severity"] = results_df["Severity"].astype(SEVERITY_CATEGORIES)
     results_df["N"] = results_df["N"].astype(int)
     results_df = results_df.set_index(["Domain", "Severity"]).sort_index()
+
+    # Format confusion matrix
+    cm = pd.DataFrame(
+        cm,
+        index=["Actual Positive", "Actual Negative"],
+        columns=["Predicted Positive", "Predicted Negative"]
+    )
+    if "healthbench-icl" in args.predictions_file.lower():
+        output_path = "confusion_matrix_healthbench_icl_omission_detection.png"
+    else:
+        output_path = "confusion_matrix_zeros_shot_omission_detection.png"
+    plot_confusion_matrix(cm, output_path=output_path)
+    logger.info(f"Confusion Matrix (All Domain), Saved to {output_path}:\n{cm}")
 
     # Log results
     with pd.option_context('display.float_format', '{:.1%}'.format):
@@ -204,38 +286,31 @@ def main():
         s = s.replace("%", "")
         logger.info(f"\nOmission Detection Results:\n{s}")
 
-    # Log a false positive and true positive example if available
+    # Log examples
+    # Pre-selected after manual review of false positive/negative errors
+    example_ids = [
+        "ovmqy_ltR3dkSSP8WPfQflN7mcpYnEjBuArX7AGcCNw=",
+        "0HeXy64xwkdVDfn5oHsNnPhWeN5dnbGkK2EowQnnZVI=",
+        "5SSZxZGP-Ld3BbJ6J5tYUJYPP5PdcVZR1rypuOEQ4A8=",
+        "4hwBx7ACHagZrJvHyT8ccJkYC3gFdCHcNZcpV7QDZf4="
+    ]
+
+    for ex_id in example_ids:
+        ex = merged_df.loc[ex_id].iloc[0]
+        logger.info(f"\nExample ID {ex_id}:\n{format_example(ex)}")
+
+    # Randomly sample examples of each error type
     shuf_examples = merged_df.sample(frac=1, random_state=42)
+    shuf_examples = shuf_examples[shuf_examples.omission_severity > "Mild"]
     fp_example = shuf_examples[(shuf_examples["label"] == False) & (shuf_examples["pred_label"] == True)].head(1)
     tp_example = shuf_examples[(shuf_examples["label"] == True) & (shuf_examples["pred_label"] == True)].head(1)
-
-    def format_example(example: pd.Series) -> str:
-        if example.empty:
-            return "No example"
-        # Format the annotated and predicted omissions as numbered lists
-        if example.n_omissions_pred > 0:
-            pred = [f"{i+1}. {omission}" for i, omission in enumerate(example.omissions_pred)]
-            pred = "\n\t".join(pred)
-        else:
-            pred = "No omissions predicted."
-        if example.n_omissions > 0:
-            gt = [f"{i+1}. {omission}" for i, omission in enumerate(example.omissions)]
-            gt = "\n\t".join(gt)
-        else:
-            gt = "No annotated omissions."
-
-        out = f"ID: {example.name} Title: {example.title}\n"
-        out += f"Question: {example.question}\n"
-        out += f"Chatbot Response ({example.model}): {example.response}\n"
-        out += f"Severity: {example.omission_severity}\n"
-        out += f"Annotated Omissions:\n\t{gt}\n"
-        out += f"Predicted Omissions:\n\t{pred}\n"
-        return out
-
+    fn_example = shuf_examples[(shuf_examples["label"] == True) & (shuf_examples["pred_label"] == False)].head(1)
     if not fp_example.empty:
         logger.info(f"\nExample False Positive:\n{format_example(fp_example.iloc[0])}")
     if not tp_example.empty:
         logger.info(f"\nExample True Positive:\n{format_example(tp_example.iloc[0])}")
+    if not fn_example.empty:
+        logger.info(f"\nExample False Negative:\n{format_example(fn_example.iloc[0])}")
 
 
 if __name__ == "__main__":
