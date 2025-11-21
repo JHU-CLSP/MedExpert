5d819b95faee941560c3fc9c4628beac063dd57e
Mahsa Yarmohammadi <mahsa.yarmohamadi@gmail.com>
2025-11-21 16:48:24 -0500

    added verify_only step

--- run_factuality_detection.sh
+++ run_factuality_detection.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python -m medscore.medscore --config ./factuality_omission_detection/medscore_config.yml --debug
+
+# If the decomposition step is completed, run only the verification step:
+# python -m medscore.medscore --config ./factuality_omission_detection/medscore_config.yml --debug --verify_only
