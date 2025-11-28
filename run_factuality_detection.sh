223acf6d81d3da807689dc606591a11c74832095
sonal-ssj <sonal.joshi.ssj@gmail.com>
2025-11-28 06:45:13 +0000

    moving files to directories

--- run_factuality_detection.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m medscore.medscore --config ./factuality_omission_detection/medscore_config.yml --debug
-
-# If the decomposition step is completed, run only the verification step:
-# python -m medscore.medscore --config ./factuality_omission_detection/medscore_config.yml --debug --verify_only
